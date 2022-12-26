fed43594ebc90fd0591002f1d66332d9a958c360
mmmmm1233212344 <abhaymilan8@gmail.com>
2022-12-26 10:49:44 +0530

    Added Image And Description Area And Added Css

--- assets/js/api.js
+++ assets/js/api.js
@@ -3,16 +3,20 @@ document.querySelector('#input').addEventListener('click', getFetch)
 
 function getFetch(event){
     event.preventDefault();
-//   const choice = document.querySelector('input').value
-  const url = 'https://api.nasa.gov/planetary/apod?api_key=XW877BDgoSCa4jkAuvS2yA1ao3VbEnTwHLHz4Qb4'
-  
+
+  const choice = document.querySelector('#choice').value
+  console.log(choice)
+  const url = `https://api.nasa.gov/planetary/apod?api_key=XW877BDgoSCa4jkAuvS2yA1ao3VbEnTwHLHz4Qb4&date=${choice}`
+// https://api.nasa.gov/planetary/apod&date
 
   console.log("hi") 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
-        console.log(data)
-            
+       let check=document.getElementById("image").src=data.url;
+            console.log(check)
+       document.getElementById("description").innerText=data.explanation;
+
         
       })
       .catch(err => {
